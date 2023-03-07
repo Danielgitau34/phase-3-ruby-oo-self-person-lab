dbd0d72922d51a04f31866199628148c6e9d64a1
Daniel Gitau <gitaud959@gmail.com>
2023-03-07 14:25:35 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -54,6 +54,19 @@ class Person
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
+      def start_conversation(friend, topic)
+        case topic 
+        when "politics"
+          [self, friend].each { |person| person.happiness -= 2 }
+          "blah blah partisan blah lobbyist"
+        when "weather"
+          [self, friend].each { |person| person.happiness += 1 }
+          "blah blah sun blah rain"
+        else
+          "blah blah blah blah blah"
+        end
+      end
+end
 
 
 
