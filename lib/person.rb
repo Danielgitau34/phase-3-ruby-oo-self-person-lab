cf77da244707ad192ccd571a4bbdbe71415b42c5
Daniel Gitau <gitaud959@gmail.com>
2023-03-07 14:19:55 +0300

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -7,4 +7,12 @@ class Person
         @bank_account = 25
         @happiness = 8
         @hygiene = 8
+    endclass Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end
